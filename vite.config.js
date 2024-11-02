5bba11f2f89fe42137c2139966d9ffb0060bcae6
Dotan Beck <dotan.beck@gmail.com>
2024-11-02 08:39:00 +0200

    added readme

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {proxy: {"/api": "http://localhost:3002"}}
+  base: '/CvApp/',  // Add this line, matching your GitHub repo name
+  server: {
+    proxy: {
+      "/api": "http://localhost:3002"
+    }
+  }
 })
